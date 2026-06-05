7d8bf7b3db476a916487a9a74385b236553e9c93
mihaitom <99483289+mihaitom@users.noreply.github.com>
2026-06-05 15:25:42 +0200

    Update Vite config to set output directory
    Removed library build configuration and set output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/power-flow-diagram/",
   build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "PowerFlow",
-      fileName: "powerflow",
-      formats: ["es", "umd"],
-    },
-    // No external deps — @mdi/js icon paths are bundled in, so a single
-    // <script> tag is enough in any framework or plain HTML.
+    outDir: "dist",
   },
 });
