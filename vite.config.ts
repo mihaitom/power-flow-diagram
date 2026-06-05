e0623731ce6975efe86d34e8ccaaacfec052afb8
mihaitom <99483289+mihaitom@users.noreply.github.com>
2026-06-05 15:18:02 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/power-flow-diagram/",
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
